51fc955661c9a0d35fdec8eac069a58ded63ae5f
Maggie Lou <margaretlou2019@u.northwestern.edu>
2016-08-02 15:24:53 +0200

    Upload exploration file.

---
+++ exploration.R
@@ -0,0 +1,33 @@
+#' Questions: 
+#' - What percentage of people pay? 
+#' - How many people don't have Apple products? 
+#' - What is the average number of collected artwork / active projects
+
+
+
+
+
+
+
+
+#' - What is the relationship between paying and user age?
+#' - relationship between total spent and : average time per session, number projects, device_type, platform ...  
+#' - relationship between project count and paying? 
+#' - relationship between usage_min and paying
+#' - relationship between project count and usage_min 
+
+
+
+
+#' Plots
+#' - corr plot with all numeric variables 
+#' - decision tree to predict paying, has downloaded artwork, etc. 
+#' - clustering 
+
+
+
+
+
+
+#' Interesting things to look into 
+#' - people whose first_seen is pretty recent and they still have purchases 
--- feature_engineering.R
+++ feature_engineering.R
@@ -5,3 +5,5 @@ feature_engineer <- function(t) {
   t <- t %>% mutate(has_collected_artwork = gp.daily_artwork_count >0, avg.events_per_session = Total_Events/Total_Sessions, avg.min_on_app = Total_Sessions/Usage_Min, User_age = as.integer(Last_Seen - First_Seen))
 }
 
+#' language matches English 
+#'  - if phone isn't in English, it must be more inconvenient for the user to read Over in English 
